bc30be3516d84989cbc47abe9ea3350bf233e2dc
rajasadeem <rajasadeem96@gmail.com>
2023-11-07 15:47:21 +0500

    strict query added in connection

--- src/loaders/mongoose.js
+++ src/loaders/mongoose.js
@@ -5,7 +5,7 @@ export default async function mongooseLoader() {
   const connection = mongoose.connection;
   connection.once("connected", () => console.log("Database Connected ~"));
   connection.on("error", (error) => console.log("Database Error: ", error));
-
+  mongoose.set("strictQuery", true);
   mongoose.connect(config.env.mongodbUri, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
